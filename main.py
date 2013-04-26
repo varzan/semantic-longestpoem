153303a26206da4335649cc8bfc1737960b710fa
Razvan Ionescu <razvan141@gmail.com>
2013-04-26 18:16:53 +0300

    * Add some plots * Fix a nasty bug in main() function that made us output wrong results * Add Banerjee-Pedersen, adjusted Banerjee-Pedersen * Remove some similarity measures that need normalization * Remove memoize decorator (it wasn't doing what I thought) and do it manually * Fix a bug in computing sum of IDFs that made some (rare) couplets have score > 1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import pickle
 import datetime
 import nltk
+import matplotlib.pyplot as plt
 from itertools import tee, izip
 from nltk.corpus import wordnet_ic
 from semsim import SimilarityCalculator, preprocess
@@ -19,12 +20,13 @@ def preprocess_lyrics(fname):
         pickle.dump(lyrics, open(pickled_fname, 'w'))
         return lyrics
 
-def print_report(f, scores, lyrics, pf):
+def print_report(f, scores, lyrics, pf, img_fname):
     results = list(zip(pairwise(lyrics), scores))
     pickle.dump(results, pf)
     for (lyric1, lyric2), score in results:
         f.write('{}\n{}\nScore: {}\n\n'.format(lyric1, lyric2, score))
     f.write('\n')
+    thresh_scores = {}
     for threshold in range(20, 85, 5):
         float_thresh = threshold / 100.0
         selected = [(lyric1, lyric2) for (lyric1, lyric2), score in results
@@ -38,15 +40,24 @@ def print_report(f, scores, lyrics, pf):
         for lyric1, lyric2 in selected:
             f.write('{}\n{}\n'.format(lyric1, lyric2))
         f.write('\n')
+        thresh_scores[float_thresh] = nselected
+    plt.clf()
+    plt.hist(scores, bins = 20, range=(0.0, 1.0))
+    plt.savefig(img_fname)
+    f.close()
+    pf.close()
+    return thresh_scores
     
 def main(fname):
     lyrics = preprocess_lyrics(fname)
     collection = nltk.TextCollection(nltk.corpus.brown)
     ic = wordnet_ic.ic('ic-brown.dat')
-    scores = []
-    for similarity in SimilarityCalculator.SIMILARITIES.keys():
+    thresh_counts = {}
+    for similarity in SimilarityCalculator.SIMILARITIES.keys() + ['bp_adj']:
+        scores = []
         output_fname = os.path.join('output', similarity + '.txt')
         pickled_fname = output_fname + '.pickled'
+        img_fname = os.path.join('output', similarity + '_hist.png')
         if os.path.exists(output_fname):
             continue
         now = datetime.datetime.now()
@@ -58,14 +69,43 @@ def main(fname):
             sc = SimilarityCalculator(collection, similarity, ic)
             for lyric1, lyric2 in pairwise(lyrics):
                 scores.append(sc.similarity_bidirectional(lyric1, lyric2))
-        print_report(open(output_fname, 'w'), scores,
-                     open(fname, 'r').read().split('\n'),
-                     open(pickled_fname, 'w'))
+            f = open('bp_backup.txt.pickled', 'w')
+            results = list(zip(pairwise(lyrics), scores))
+            pickle.dump(results, f)
+            f.close()
+        thresh_counts[similarity] = print_report(open(output_fname, 'w'),
+                                                 scores,
+                                                 open(fname, 'r').read().
+                                                 split('\n'),
+                                                 open(pickled_fname, 'w'),
+                                                 img_fname)
         now = datetime.datetime.now()
-        print '[{}]: Finished calculation on {}'.format(str(now), similarity)
+        print '[{}] Finished calculation on {}'.format(str(now), similarity)
+    plt.clf()
+    for similarity in thresh_counts.keys():
+        res = list(thresh_counts[similarity].iteritems())
+        res.sort()
+        res = zip(*res)
+        plt.plot(res[0], res[1], label=similarity, zorder=1)
+        plt.scatter(res[0], res[1], zorder=2)
+    plt.legend()
+    plt.xlabel("threshold")
+    plt.ylabel("no. lyrics selected")
+    plt.savefig(os.path.join("output", "thresholds.png"))
+    print thresh_counts['wup']
+    print thresh_counts['bp']
     
-
+def adjust_bp():
+    bp_fname = os.path.join('output', 'bp.txt.pickled')
+    bp_adj_fname = os.path.join('output', 'bp_adj.txt.pickled')
+    results = pickle.load(open(bp_fname, 'r'))
+    scores = [score for couplet, score in results]
+    couplets = [couplet for couplet, score in results]
+    scores = [score * 4 for score in scores]
+    pickle.dump(zip(couplets, scores), open(bp_adj_fname, 'w'))
+    
 if __name__ == "__main__":
+    adjust_bp()
     main("longestpoem.txt")
     
 
--- semsim.py
+++ semsim.py
@@ -1,6 +1,7 @@
 import string
 import re
 import nltk
+import bp_wsd
 from nltk.corpus import wordnet as wn
 from nltk.corpus import wordnet_ic
 from nltk.decorators import memoize
@@ -20,11 +21,14 @@ def preprocess(text):
 class SimilarityCalculator:
 
     SIMILARITIES = {'path' : lambda s1, s2, ic: s1.path_similarity(s2),
-                    'lch' : lambda s1, s2, ic: s1.lch_similarity(s2),
+                    #'lch' : lambda s1, s2, ic: s1.lch_similarity(s2),
                     'wup' : lambda s1, s2, ic: s1.wup_similarity(s2),
-                    'res' : lambda s1, s2, ic: s1.res_similarity(s2, ic),
-                    'jcn' : lambda s1, s2, ic: s1.jcn_similarity(s2, ic),
-                    'lin' : lambda s1, s2, ic: s1.path_similarity(s2, ic)}
+                    #'res' : lambda s1, s2, ic: s1.res_similarity(s2, ic),
+                    #'jcn' : lambda s1, s2, ic: s1.jcn_similarity(s2, ic),
+                    'lin' : lambda s1, s2, ic: s1.lin_similarity(s2, ic),
+                    'bp' : lambda s1, s2, ic:
+                           bp_wsd.relatedness(s1, s2, bp_wsd.relpairs[s1.pos],
+                                              normalize=True)}
 
     def __init__(self, collection, sim_measure, ic):
         self.pos_tags = ['N', 'V']
@@ -32,21 +36,29 @@ class SimilarityCalculator:
         self.collection = collection
         self.sim_measure = SimilarityCalculator.SIMILARITIES[sim_measure]
         self.ic = ic
+        self.cache = {}
 
-    @memoize
     def word_similarity(self, word1, word2, pos_tag):
-        pos_tag = self.wn_pos_tags[self.pos_tags.index(pos_tag)]
-        synsets1 = wn.synsets(word1, pos_tag)
-        synsets2 = wn.synsets(word2, pos_tag)
-        if not synsets1 or not synsets2:
-            return 0
-        sim = max(self.sim_measure(sense1, sense2, self.ic) or 0
-                   for sense1 in synsets1 for sense2 in synsets2)
-        return sim
+        if (word1, word2, pos_tag) in self.cache:
+            return self.cache[(word1, word2, pos_tag)]
+        elif (word2, word1, pos_tag) in self.cache:
+            return self.cache[(word2, word1, pos_tag)]
+        else:
+            wn_pos_tag = self.wn_pos_tags[self.pos_tags.index(pos_tag)]
+            synsets1 = wn.synsets(word1, wn_pos_tag)
+            synsets2 = wn.synsets(word2, wn_pos_tag)
+            if not synsets1 or not synsets2:
+                sim = 0
+            else:
+                sim = max(self.sim_measure(sense1, sense2, self.ic) or 0
+                           for sense1 in synsets1 for sense2 in synsets2)
+            self.cache[(word1, word2, pos_tag)] = sim
+            return sim
 
     def max_similarity(self, word, words, pos_tag):
         a = max(self.word_similarity(word, other, pos_tag)
                    for other in words) if words else 0
+        print word, words, a
         return a
 
     def similarity(self, *sentences):
@@ -54,13 +66,11 @@ class SimilarityCalculator:
         for tag in self.pos_tags:
             pos_sets[tag] = [[word for word, pos_tag in sentence
                               if pos_tag == tag] for sentence in sentences]
-        idf = {}
-        for tag in self.pos_tags:
-            for word in pos_sets[tag][0]:
-                idf[word] = self.collection.idf(word)
-        sim = sum(sum(self.max_similarity(word, pos_sets[tag][1], tag) * idf[word]
+        sim = sum(sum(self.max_similarity(word, pos_sets[tag][1], tag) *
+                      self.collection.idf(word)
                       for word in pos_sets[tag][0]) for tag in self.pos_tags)
-        denom = sum(idf.values())
+        denom = sum(sum(self.collection.idf(word) for word in pos_sets[tag][0])
+                    for tag in self.pos_tags)
         if denom > 0:
             sim /= denom
         return sim
@@ -72,7 +82,7 @@ class SimilarityCalculator:
 def test():
     col = nltk.TextCollection(nltk.corpus.brown)
     brown_ic = wordnet_ic.ic('ic-brown.dat')
-    sc = SimilarityCalculator(col, 'wup', brown_ic)
+    sc = SimilarityCalculator(col, 'bp', brown_ic)
     sentence1 = preprocess("The jurors were taken into the courtroom in groups of 40 and asked to fill out a questionnaire.")
     sentence2 = preprocess("About 120 potential jurors were being asked to complete a lengthy questionnaire.")
     print sc.similarity_bidirectional(sentence1, sentence2)
